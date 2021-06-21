9b3f28e40e54cb38dabd77268d5fbe02b3989db1
ppopkov <popkov@foodgram.store>
2021-06-21 16:13:09 +0300

    Добавьте файлы проекта.

---
+++ Entities/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Models
+{
+    class User : IdentityUser
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IdentityRoleId { get; set; }
+        public IdentityRole IdentityRole { get; set; }
+        
+        public byte[] Avatar { get; set; }
+        public string GetAvatar => (Avatar is null) ? "" : $"data:image/jpeg;base64,{Convert.ToBase64String(Avatar)}";
+
+        public DateTimeOffset DateOfRegister { get; set; } = DateTimeOffset.UtcNow;
+
+    }
+}
